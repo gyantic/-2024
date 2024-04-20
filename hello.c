e3ebcc6fba2246c62fd175fd08dddb98232ba7dd
kyuta <kkimura330mp@gmail.com>
2024-04-20 13:46:54 +0900

    intial

--- hello.c
+++ hello.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char * argv[]) {
-    int i;
+int main(int argc, char *argv[])
+{
+  int i;
 
-    for(i=0 ; i<10 ; i++) {
-        printf("%d\n", i);
-    }
+  for (i = 0; i < 10; i++)
+  {
+    printf("%d\n", i);
+  }
 
-    return 0;
+  return 0;
 }
